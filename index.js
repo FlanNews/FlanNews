dd13870039836a9035617fd60a8bccad645b195e
FlanNews <166428385+FlanNews@users.noreply.github.com>
2024-04-21 21:02:54 -0600

    1.2.0

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {sataAndagi()};
 
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
+var topnav = document.getElementById("topnav1");
+var sticky = topnav.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+function sataAndagi() {
+  if (window.scrollY >= sticky) {
+    topnav.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    topnav.classList.remove("sticky");
   }
+}
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("CarouselSlide");
+  let dots = document.getElementsByClassName("ProgressDot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
